728af80c07d367afa28d05c35a0804acc66db04a
Feric Decenan <feric08c@gmail.com>
2024-02-25 15:19:12 +0800

    changes

--- MoneyBankV1/MainForm.Designer.cs
+++ MoneyBankV1/MainForm.Designer.cs
@@ -32,8 +32,9 @@
             this.panel4 = new System.Windows.Forms.Panel();
             this.ucTransactionList1 = new MoneyBankV2.ucTransactionList();
             this.dbPanel1.SuspendLayout();
+            this.dbPanel6.SuspendLayout();
             this.dbPanel3.SuspendLayout();
-            this.dbPanel8.SuspendLayout();
+            this.dbPanel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -47,20 +48,62 @@
             // dbPanel6
             // 
             this.dbPanel6.BackColor = System.Drawing.Color.MidnightBlue;
-            this.dbPanel6.Location = new System.Drawing.Point(0, 436);
+            this.dbPanel6.Size = new System.Drawing.Size(230, 100);
             // 
             // dbPanel3
             // 
             this.dbPanel3.BackColor = System.Drawing.Color.DarkBlue;
             this.dbPanel3.Controls.Add(this.panel1);
-            this.dbPanel3.Size = new System.Drawing.Size(200, 436);
+            this.dbPanel3.Location = new System.Drawing.Point(0, 100);
+            this.dbPanel3.Size = new System.Drawing.Size(230, 396);
             // 
             // dbPanel8
             // 
-            this.dbPanel8.Controls.Add(this.panel4);
-            this.dbPanel8.Controls.Add(this.panel3);
-            this.dbPanel8.Controls.Add(this.panel2);
-            this.dbPanel8.Size = new System.Drawing.Size(750, 496);
+            this.dbPanel8.Size = new System.Drawing.Size(952, 496);
+            // 
+            // Username
+            // 
+            this.Username.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.ForeColor = System.Drawing.Color.White;
+            this.Username.Size = new System.Drawing.Size(24, 23);
+            // 
+            // CurrentTime
+            // 
+            this.CurrentTime.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentTime.ForeColor = System.Drawing.Color.White;
+            this.CurrentTime.Location = new System.Drawing.Point(80, 64);
+            this.CurrentTime.Size = new System.Drawing.Size(24, 23);
+            // 
+            // CurrentDate
+            // 
+            this.CurrentDate.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentDate.ForeColor = System.Drawing.Color.White;
+            this.CurrentDate.Location = new System.Drawing.Point(81, 31);
+            this.CurrentDate.Size = new System.Drawing.Size(24, 23);
+            // 
+            // dbcLabelDesc2
+            // 
+            this.dbcLabelDesc2.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dbcLabelDesc2.ForeColor = System.Drawing.Color.White;
+            this.dbcLabelDesc2.Location = new System.Drawing.Point(9, 64);
+            this.dbcLabelDesc2.Size = new System.Drawing.Size(44, 23);
+            // 
+            // dbcLabelDesc1
+            // 
+            this.dbcLabelDesc1.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dbcLabelDesc1.ForeColor = System.Drawing.Color.White;
+            this.dbcLabelDesc1.Location = new System.Drawing.Point(9, 30);
+            this.dbcLabelDesc1.Size = new System.Drawing.Size(44, 23);
+            // 
+            // cLabelDesc1
+            // 
+            this.cLabelDesc1.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cLabelDesc1.ForeColor = System.Drawing.Color.White;
+            this.cLabelDesc1.Size = new System.Drawing.Size(80, 23);
+            // 
+            // dbPanel2
+            // 
+            this.dbPanel2.Size = new System.Drawing.Size(232, 498);
             // 
             // panel1
             // 
@@ -69,7 +112,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(198, 434);
+            this.panel1.Size = new System.Drawing.Size(228, 394);
             this.panel1.TabIndex = 0;
             // 
             // ucMainMenu1
@@ -77,17 +120,18 @@
             this.ucMainMenu1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ucMainMenu1.Location = new System.Drawing.Point(0, 0);
             this.ucMainMenu1.Name = "ucMainMenu1";
-            this.ucMainMenu1.Size = new System.Drawing.Size(198, 434);
+            this.ucMainMenu1.Size = new System.Drawing.Size(228, 394);
             this.ucMainMenu1.TabIndex = 0;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel2.Controls.Add(this.cLabelTitle1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Location = new System.Drawing.Point(232, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(750, 100);
+            this.panel2.Size = new System.Drawing.Size(722, 100);
             this.panel2.TabIndex = 0;
             // 
             // cLabelTitle1
@@ -97,9 +141,9 @@
             this.cLabelTitle1.ForeColor = System.Drawing.Color.White;
             this.cLabelTitle1.Location = new System.Drawing.Point(0, 0);
             this.cLabelTitle1.Name = "cLabelTitle1";
-            this.cLabelTitle1.Size = new System.Drawing.Size(750, 100);
+            this.cLabelTitle1.Size = new System.Drawing.Size(720, 98);
             this.cLabelTitle1.TabIndex = 0;
-            this.cLabelTitle1.Text = "Money Bank Information System";
+            this.cLabelTitle1.Text = "Bank Management Information System";
             this.cLabelTitle1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel3
@@ -107,9 +151,9 @@
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel3.Controls.Add(this.ucReports1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel3.Location = new System.Drawing.Point(550, 100);
+            this.panel3.Location = new System.Drawing.Point(754, 100);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(200, 396);
+            this.panel3.Size = new System.Drawing.Size(200, 398);
             this.panel3.TabIndex = 1;
             // 
             // ucReports1
@@ -117,7 +161,7 @@
             this.ucReports1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ucReports1.Location = new System.Drawing.Point(0, 0);
             this.ucReports1.Name = "ucReports1";
-            this.ucReports1.Size = new System.Drawing.Size(198, 394);
+            this.ucReports1.Size = new System.Drawing.Size(198, 396);
             this.ucReports1.TabIndex = 0;
             // 
             // panel4
@@ -125,9 +169,9 @@
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel4.Controls.Add(this.ucTransactionList1);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel4.Location = new System.Drawing.Point(0, 100);
+            this.panel4.Location = new System.Drawing.Point(232, 100);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(550, 396);
+            this.panel4.Size = new System.Drawing.Size(522, 398);
             this.panel4.TabIndex = 2;
             // 
             // ucTransactionList1
@@ -135,7 +179,7 @@
             this.ucTransactionList1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ucTransactionList1.Location = new System.Drawing.Point(0, 0);
             this.ucTransactionList1.Name = "ucTransactionList1";
-            this.ucTransactionList1.Size = new System.Drawing.Size(548, 394);
+            this.ucTransactionList1.Size = new System.Drawing.Size(520, 396);
             this.ucTransactionList1.TabIndex = 0;
             // 
             // MainForm
@@ -143,12 +187,22 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(954, 498);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Location = new System.Drawing.Point(0, 0);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            this.Controls.SetChildIndex(this.dbPanel1, 0);
+            this.Controls.SetChildIndex(this.dbPanel2, 0);
+            this.Controls.SetChildIndex(this.panel2, 0);
+            this.Controls.SetChildIndex(this.panel3, 0);
+            this.Controls.SetChildIndex(this.panel4, 0);
             this.dbPanel1.ResumeLayout(false);
+            this.dbPanel6.ResumeLayout(false);
+            this.dbPanel6.PerformLayout();
             this.dbPanel3.ResumeLayout(false);
-            this.dbPanel8.ResumeLayout(false);
+            this.dbPanel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
