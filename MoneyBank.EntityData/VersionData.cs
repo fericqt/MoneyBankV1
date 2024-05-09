72be016682585c091ed9f98daac4ac7ad5b3a146
fericqt <feric08c@gmail.com>
2024-05-09 12:45:03 +0800

    formatting

--- MoneyBank.EntityData/VersionData.cs
+++ MoneyBank.EntityData/VersionData.cs
@@ -13,59 +13,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MoneyBank.EntityData
-{
-    public class VersionData : BaseDBEntity<moneybankEntities, Conn, VersionDTO, long>,IEntityData<tblversion, long>
-    {
-        public VersionData()
-        {
+namespace MoneyBank.EntityData {
+    public class VersionData : BaseDBEntity<moneybankEntities, Conn, VersionDTO, long>, IEntityData<tblversion, long> {
+        public VersionData() {
         }
 
-        public VersionData(moneybankEntities ts) : base(ts)
-        {
+        public VersionData(moneybankEntities ts) : base(ts) {
         }
 
-        public VersionData(Conn conn) : base(conn)
-        {
+        public VersionData(Conn conn) : base(conn) {
         }
 
-        public IEnumerable<tblversion> GetAll()
-        {
+        public IEnumerable<tblversion> GetAll() {
             return _ts.tblversions.ToList();
         }
 
-        public tblversion GetById(long id)
-        {
-            return _ts.tblversions.FirstOrDefault(c=>c.IdTrack == id);
+        public tblversion GetById(long id) {
+            return _ts.tblversions.FirstOrDefault(c => c.IdTrack == id);
         }
 
-        public string GetNewID()
-        {
+        public string GetNewID() {
             throw new NotImplementedException();
         }
-        protected override void DeleteData(long id)
-        {
+        protected override void DeleteData(long id) {
             throw new NotImplementedException();
         }
 
-        protected override void SaveData(VersionDTO myDTO)
-        {
-           var tbl = new CMapping<VersionDTO, tblversion>().GetMappingResult(myDTO);
-            _ts.tblversions.Add(tbl);   
+        protected override void SaveData(VersionDTO myDTO) {
+            var tbl = new CMapping<VersionDTO, tblversion>().GetMappingResult(myDTO);
+            _ts.tblversions.Add(tbl);
             _ts.SaveChanges();
         }
 
-        protected override void SetTables()
-        {
+        protected override void SetTables() {
             _tableName = "tblversion";
         }
 
-        protected override void UpdateData(VersionDTO myDTO)
-        {
+        protected override void UpdateData(VersionDTO myDTO) {
             var tbl = GetById(myDTO.IdTrack);
             var tblNew = new CMapping<VersionDTO, tblversion>().GetMappingResult(myDTO);
-            if(tbl.Version == tblNew.Version)
-            {
+            if (tbl.Version == tblNew.Version) {
                 throw new ArgumentException("No changes have been made to the version.\nPlease update the version.");
             }
             var oUpdate = $"Version: {tbl.Version}\n{tbl.NewUpdates}";
